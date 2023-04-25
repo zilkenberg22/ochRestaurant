ca8e79a29e5da34160bc994b95f0860944d28eeb
dorjvanchig <batbold.dorjo@gmail.com>
2023-04-25 20:42:36 +0800

    ss

--- src/miniiSags/baraaSagslakh.js
+++ src/miniiSags/baraaSagslakh.js
@@ -62,6 +62,7 @@ export default function BaraaSagslakh() {
   }
 
   function utsgay(ugugdul) { 
+    console.log(ugugdul)
     let index = barimt.baraanuud.findIndex(a=> a.baarKodniiKhoch === ugugdul.baarKodniiKhoch)
     if (index > -1)
         barimt.baraanuud.splice(index, 1)
